a572247da17123a93d2c7a8bf2fdbefe6c326450
Resul Dinc <dincresull@gmail.com>
2024-08-09 15:05:27 +0300

    create model and dbconnection

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CachingWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CachingWebApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
---
+++ Models/Driver.cs
@@ -0,0 +1,9 @@
+﻿namespace CachingWebApi.Models
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int DriverNumber { get; set; }
+    }
+}
