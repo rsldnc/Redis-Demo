01e9fc97658269f7e8086bfd5201fcb6e8f5bf54
Resul Dinc <dincresull@gmail.com>
2024-08-09 15:49:48 +0300

    create controller

---
+++ Controllers/DriversController.cs
@@ -0,0 +1,72 @@
+﻿using CachingWebApi.Context;
+using CachingWebApi.Models;
+using CachingWebApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CachingWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriversController : ControllerBase
+    {
+        private readonly ICacheService _cacheService;
+        private readonly ILogger<DriversController> _logger;
+        private readonly AppDbContext _context;
+
+        public DriversController(ILogger<DriversController> logger, ICacheService cacheService, AppDbContext context)
+        {
+            _logger = logger;
+            _cacheService = cacheService;
+            _context = context;
+        }
+
+        [HttpGet("drivers")]
+        public async Task<IActionResult> Get()
+        {
+            var cacheData = _cacheService.GetData<IEnumerable<Driver>>("drivers");
+
+            if (cacheData != null && cacheData.Count() > 0)
+                return Ok(cacheData);
+
+            cacheData = await _context.Drivers.ToListAsync();
+
+            var expTime = DateTimeOffset.Now.AddSeconds(30);
+            _cacheService.SetData<IEnumerable<Driver>>("drivers", cacheData, expTime);
+
+            return Ok(cacheData);
+        }
+
+
+        [HttpPost("AddDriver")]
+        public async Task<IActionResult> Post(Driver value)
+        {
+            var addedObj = await _context.Drivers.AddAsync(value);
+
+            var expTime = DateTimeOffset.Now.AddSeconds(30);
+            _cacheService.SetData<Driver>($"driver{value.Id}", addedObj.Entity,expTime);
+
+            await _context.SaveChangesAsync();
+
+            return Ok(addedObj.Entity);
+        }
+
+        [HttpDelete("DeleteDriver")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var exist = await _context.Drivers.FirstOrDefaultAsync(x => x.Id == id);
+
+            if(exist != null)
+            {
+                _context.Remove(exist);
+                await _context.SaveChangesAsync();
+
+                _cacheService.RemoveData($"driver{id}");
+
+                return NoContent();
+            }
+            return NotFound();
+        }
+    }
+}
